f314cf6e4d5e2ad86be6dcb7d606a3e41d339bcc
hamza <hsehnoun@hotmail.com>
2018-01-17 11:27:44 +0100

    envoi de version

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,22 @@
-#write your code here
+def add(number1, number2)
+ 	number1 + number2
+end
+
+def factorial(number)
+	(1..number).reduce(1, :*)
+end
+def multiply(*args)
+	args.inject(:*) 
+end
+
+def power(num_1, num_2)
+  num_1 ** num_2
+end
+
+def subtract(number3, number4)
+	number3 - number4
+end
+
+def sum(numbers)
+	numbers.sum
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(message)
+  message
+end
+
+def first_word(string)
+  string.partition(' ').first
+end
+
+def repeat(message, repeat = 2)
+   output = ""
+  repeat.times {
+    output += output.length > 0 ? ' ' + message : message
+  }
+  output
+end
+
+def start_of_word(word, length = 1)
+  word[0...length]
+end
+
+def shout(message)
+  message.upcase
+end
+
+def titleize(string)
+  do_not_capitalize = ['a', 'an', 'and', 'in', 'of', 'or', 'over', 'the']
+  output = string.split(/ /).each_with_index.map do |word, index|
+    if index > 0 && (do_not_capitalize.include? word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  output.join(' ')
+end#write your code here
